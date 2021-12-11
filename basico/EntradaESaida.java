1a2c456049a07d3dc350b17fe0b90b7c92e3b546
Gilton Gil <giltongil11@gmail.com>
2021-12-11 19:12:41 -0300

    primeio commit

---
+++ basico/EntradaESaida.java
@@ -0,0 +1,38 @@
+package cursoJava.basico;
+
+import java.util.Scanner;
+
+public class EntradaESaida {
+
+	public static void main(String[] args) {
+		
+		//Ciação de variáveis
+		String nome;
+		int idade;
+		double altura;
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome: ");
+		
+		nome = leitor.nextLine();
+		
+		System.out.println("Digite sua idade: ");
+		
+		idade = leitor.nextInt();
+		
+		System.out.println("Digite sua altura: ");
+		
+		altura = leitor.nextDouble();
+		
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(altura);
+		System.out.println("modificado");
+		System.out.println("outra modificao");
+		System.out.println("outra modificado mais um vez");
+		
+
+	}
+
+}
---
+++ basico/OlaMundo.java
@@ -0,0 +1,19 @@
+package cursoJava.basico;
+/*
+ * Classe que imprime um texto no console
+ * 
+ * versão 1.0
+ * 
+ * autor: Gilton Matos
+ * 
+ * */
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá Mundo da Programação!");
+
+	}
+
+}
